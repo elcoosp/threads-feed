90eab6db7fb7900e22c8a5efda8d938b4c413b8d
Elcoosp <elcoosp@gmail.com>
2018-05-07 00:35:00 +0200

    add: HandledSnapsMap
    Take a query and a component to render. Create a list of the component populated with the packed (as an array) snapshots from the query.

--- src/components/PackedSnaps.js
+++ src/components/PackedSnaps.js
@@ -2,24 +2,38 @@ import React, { Component } from 'react'
 import { packSnaps } from '../utils'
 class PackedSnaps extends Component {
   state = {
-    data: []
+    data: null,
+    error: null
   }
   set = key => data => this.setState(state => ({ [key]: data }))
 
   componentDidMount = () =>
-    (this.unsubscribe = packSnaps(this.props.query, this.set('data')))
+    (this.unsubscribe = packSnaps(
+      this.props.query,
+      this.set('data'),
+      this.set('error')
+    ))
 
   componentWillUnmount = () => this.unsubscribe()
 
   render() {
     const Loader = this.props.loader
-
-    return this.state.data.length > 0 ? (
-      this.props.children(this.state.data)
-    ) : (
-      <Loader />
-    )
+    const { error, data } = this.state
+    return error || data ? this.props.children({ error, data }) : <Loader />
   }
 }
 
+// TODO: pass config for components
+export const HandledSnapsMap = ({ query, mapTo }) => (
+  <PackedSnaps loader={() => <span>Loading</span>} query={query}>
+    {({ data, error }) => {
+      const Component = mapTo
+      return data ? (
+        <div>{data.map(doc => <Component key={doc.id} {...doc} />)}</div>
+      ) : (
+        error && <span>An error occured</span>
+      )
+    }}
+  </PackedSnaps>
+)
 export default PackedSnaps
