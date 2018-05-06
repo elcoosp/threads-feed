43e93b3f678cf801011f80ff990be0173dd4487e
Elcoosp <elcoosp@gmail.com>
2018-05-06 22:18:06 +0200

    add: form to add a thread

---
+++ src/components/NewThreadForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import Formalized from './Formalized'
+import { authorizations } from './withAuthorization'
+import { db } from '../firebase/firebase'
+
+class NewThreadForm extends Component {
+  state = {
+    error: null,
+    success: null
+  }
+
+  setStateKey = key => data => this.setState(prevState => ({ [key]: data }))
+  onSubmit = values => {
+    const { feedName, threadLink } = values
+    const { history } = this.props
+    const feedRef = db.collection('feeds').doc(feedName)
+
+    feedRef.set({ name: feedName, new: true })
+    feedRef
+      .collection('threads')
+      .add({ threadLink })
+      .then(resp => this.setState(prevState => ({ success: true })))
+      .catch(e => this.setState(prevState => ({ error: e })))
+  }
+
+  render() {
+    const { error, success } = this.state
+    return (
+      <div>
+        <Formalized
+          pickFields={({ feedName, threadLink }) => [feedName, threadLink]}
+          submit={this.onSubmit}
+        />
+        {error && <span>{error}</span>}
+        {success && <span>Succesfully sended form</span>}
+      </div>
+    )
+  }
+}
+
+export default NewThreadForm
