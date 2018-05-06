fce360acba3bc55ff6bfbc931ac9fd1f2ae5fcfc
Elcoosp <elcoosp@gmail.com>
2018-05-07 00:33:04 +0200

    fix: error handling callback on packSnaps

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
-export const packSnaps = (query, cb) =>
+export const packSnaps = (query, cb, errorCb) =>
   query.onSnapshot(snaps => {
     let packedSnaps = []
     snaps.forEach(
       snap => snap.exists && packedSnaps.push({ ...snap.data(), id: snap.id })
     )
     cb(packedSnaps)
-  })
+  }, errorCb)
